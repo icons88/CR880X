8fd967551ee519fd30bf7c93bd88a64a319f0f17
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-09 22:43:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-ssl/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-ssl-openssl/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-light/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-nginx/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-ssl/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-ssl-openssl/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-light/Makefile
